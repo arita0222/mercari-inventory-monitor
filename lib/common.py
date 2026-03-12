45c5c2e6570133b19fba80ff2bcdc4543969014a
arita0222 <55143984+arita0222@users.noreply.github.com>
2026-03-12 18:31:47 +0900

    Update common.py

--- lib/common.py
+++ lib/common.py
@@ -19,6 +19,7 @@ logger = logging.getLogger("ebay_lister")
 
 # 環境変数
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
 EBAY_APP_ID = os.environ.get("EBAY_APP_ID", "")
 EBAY_DEV_ID = os.environ.get("EBAY_DEV_ID", "")
 EBAY_CERT_ID = os.environ.get("EBAY_CERT_ID", "")
